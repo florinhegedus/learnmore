3f51284138254b532aa137bb4a64808ba7a3992b
Florin Hegedus <florinxhgds@gmail.com>
2023-03-12 10:14:20 +0200

    added implementation for neuron, layer, mlp

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learnmore
 In this repository I will implement from scratch machine learning algorithms. This repository is inspired from Andrej Karpathy's [micrograd](https://github.com/karpathy/micrograd).
+```
+cd learnmore
+conda create --name learnmore --file requirements.txt
+conda activate learnmore
+```
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from engine import Value
+from utils import draw_dot
 
 
 def main_loop():
-    a = Value(2)
+    a = Value(-2)
     b = Value(3)
-    c = a + b
-    c.backward()
+    c = Value(4)
+    d = Value(5)
+    res = a.relu() ** 3 + b * c + d
+    res.backward()
+    print(res)
+    dot = draw_dot(res)
+    dot.render('graphs/gout')
     
 
 if __name__ == '__main__':
